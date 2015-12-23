39d83566db7430f83fef24669c8baf74b6b395c0
Junichi Ito <jit@sonicgarden.jp>
2015-12-24 07:22:36 +0900

    Show bookmark link as image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def bookmark_url(entry_url)
-    s_prefix = 's/' if entry_url =~ /\Ahttps/
-    "http://b.hatena.ne.jp/entry/#{s_prefix}#{entry_url.gsub(/\Ahttps?:\/\//, '')}"
-  end
 end
