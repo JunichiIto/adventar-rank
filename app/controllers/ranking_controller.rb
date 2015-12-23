de5fdac8fb92c9ab61407abbaa1a847fa4a599c3
Junichi Ito <jit@sonicgarden.jp>
2015-12-23 20:45:48 +0900

    Refactor routing

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,15 +1,13 @@
 class RankingController < ApplicationController
-  def new
-  end
-
-  def collect
-    @adventar_info = BookmarkCollector.new(params[:url]).collect_adventar_info
-    render 'new'
+  def rank
+    if params[:url].present?
+      @adventar_info = BookmarkCollector.new(params[:url]).collect_adventar_info
+    end
   rescue => e
     message = e.message.force_encoding("utf-8").truncate(50)
     Rails.logger.error message
     Rails.logger.error e.backtrace.join("\n")
     flash.now[:alert] = "エラーが発生しました。（#{message}）"
-    render 'new', status: :internal_server_error
+    render 'rank', status: :internal_server_error
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'ranking#new'
-  get '/ranking', to: 'ranking#collect'
+  root to: 'ranking#rank'
 end
