9c63af93c7138bc1571d78935d2980813adaaa28
Junichi Ito <jit@sonicgarden.jp>
2015-12-23 20:43:17 +0900

    Add feature spec on error

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -5,5 +5,11 @@ class RankingController < ApplicationController
   def collect
     @adventar_info = BookmarkCollector.new(params[:url]).collect_adventar_info
     render 'new'
+  rescue => e
+    message = e.message.force_encoding("utf-8").truncate(50)
+    Rails.logger.error message
+    Rails.logger.error e.backtrace.join("\n")
+    flash.now[:alert] = "エラーが発生しました。（#{message}）"
+    render 'new', status: :internal_server_error
   end
 end
--- spec/features/collect_bookmarks_spec.rb
+++ spec/features/collect_bookmarks_spec.rb
@@ -11,4 +11,16 @@ feature 'Collect bookmarks' do
       expect(page).to have_link '家族で東京からベトナムに移り住んだエンジニアの話 - The longest day in my life', href: 'http://detham.tumblr.com/post/134844924754/%E5%AE%B6%E6%97%8F%E3%81%A7%E6%9D%B1%E4%BA%AC%E3%81%8B%E3%82%89%E3%83%99%E3%83%88%E3%83%8A%E3%83%A0%E3%81%AB%E7%A7%BB%E3%82%8A%E4%BD%8F%E3%82%93%E3%81%A0%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%81%AE%E8%A9%B1'
     end
   end
+
+  scenario 'not found URL' do
+    visit root_url
+    fill_in 'URL', with: 'http://www.adventar.org/calendars/999999999'
+
+    VCR.use_cassette 'features/collect_bookmarks/not_found' do
+      expect(Rails.logger).to receive(:error).twice
+      click_button '送信'
+      expect(page).to have_content 'エラーが発生しました。'
+      expect(page).to have_http_status 500
+    end
+  end
 end
